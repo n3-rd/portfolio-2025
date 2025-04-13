31f5f70590ceb49aec66bc10df9d9efd6ebdef91
n3-rd <godwinjdayo@gmail.com>
2025-04-13 02:12:37 +0100

    Added marquee components and dependencies for enhanced text animation. Introduced MainMarquee.svelte for marquee display and updated +page.svelte to integrate it. Updated package.json and pnpm-lock.yaml with new marquee libraries.

---
+++ src/lib/components/MainMarquee.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Marquee } from "@selemondev/svelte-marquee";
+    import "@selemondev/svelte-marquee/dist/style.css";
+</script>
+
+<Marquee  class=" [--duration:5s] [--gap:6rem] bg-red-500 py-7 text-7xl">
+    <div>WEB.</div>
+    <div>DESIGN.</div>
+    <div>DEVELOPMENT.</div>
+    <div>CONTENT.</div>
+    <div>DIGITAL.</div>
+    <div>PORTFOLIO.</div>
+</Marquee>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,44 @@
-<div class="h-screen min-w-full flex items-end justify-center">
-  <h1 class="text-[10vw] main-test tracking-tighter" id="main-test">GODWIN JEMEGAH</h1>
+<script>
+	import MainMarquee from "$lib/components/MainMarquee.svelte";
+  import { Marquee } from "@joycostudio/marquee";
+	import { animate, stagger } from "motion";
+	import { onMount } from "svelte";
+
+  let mainText = "GODWIN\nJEMEGAH".split('\n');
+  
+  onMount(() => {
+    // Get all the letter elements
+    const elements = document.querySelectorAll('.text-letter');
+    
+    // Animate each letter from hidden to visible with a stagger
+    animate(elements, 
+      { 
+        y: [600, 0]
+      },
+      { 
+        duration: 1.5,
+        delay: stagger(0.1),
+        type: "spring",
+        stiffness: 300,
+        damping: 60
+      }
+    );
+  });
+
+</script>
+<div class="h-screen min-w-full flex items-end justify-center select-none">
+
+<div class="absolute top-[40%] left-0 w-full z-[9999]">
+  <MainMarquee />
 </div>
 
-<script>
-  
-</script>
+  <p class="text-[10vw] main-test tracking-tighter overflow-hidden" id="main-test">
+    {#each mainText as line}
+      <span class="inline-block px-4">
+        {#each line as char}
+          <span class="text-letter translate-y-[30rem] inline-block">{char}</span>
+        {/each}
+      </span>
+    {/each}
+  </p>
+</div>
