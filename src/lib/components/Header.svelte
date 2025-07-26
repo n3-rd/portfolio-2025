7b5611c8edf72352b775359df0bad3da6ce242d7
n3-rd <godwinjdayo@gmail.com>
2025-07-27 00:33:22 +0100

    header fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
 	$: isHomepage = $page.url.pathname === '/';
 </script>
 
-<div class="{isHomepage ? 'relative' : 'fixed'} w-full top-0 left-0 z-50 flex items-center justify-between px-3 py-2 sm:p-3 lg:p-4 overflow-hidden">
+<div class="fixed w-full top-0 left-0 z-50 flex items-center justify-between px-3 py-2 sm:p-3 lg:p-4 overflow-hidden">
   <a href="/" class="header-logo   sm:text-xl lg:text-2xl xl:text-3xl flex-shrink-0">G</a> 
   <div class="menu flex items-center gap-2 sm:gap-3 lg:gap-4 xl:text-3xl sm:text-sm lg:text-3xl overflow-hidden">
     <button class="menu-item hover:text-red-500 transition-all duration-300 py-1 sm:px-1 sm:py-2 lg:px-0 lg:py-0 whitespace-nowrap"
